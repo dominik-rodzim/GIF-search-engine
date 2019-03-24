6155d11801ed0a5a831c77a1b8d2a5a42ec53818
dominik-rodzim <46093484+dominik-rodzim@users.noreply.github.com>
2019-03-24 16:47:13 +0100

    Change Ajax request by adding promise

--- components/App.js
+++ components/App.js
@@ -26,26 +26,34 @@ App = React.createClass({
     );
   },
 
-  getGif: function(searchingText, callback) {
-    var url =
+  getGif: searchingText => {
+      const url =
       GIPHY_API_URL +
       "/v1/gifs/random?api_key=" +
       GIPHY_PUB_KEY +
       "&tag=" +
       searchingText;
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText).data;
-        var gif = {
-          url: data.fixed_width_downsampled_url,
-          sourceUrl: data.url
-        };
-        callback(gif);
-      }
-    };
-    xhr.send();
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        if (this.status === 200) {
+          var data = JSON.parse(this.responseText).data;
+          var gif = {
+            url: data.fixed_width_downsampled_url,
+            sourceUrl: data.url
+          };
+          resolve(gif);
+        } else {
+          reject(new Error(this.statusText));
+        }
+      };
+      xhr.onerror = function() {
+        reject(new Error(
+          `XMLHttpRequest Error: ${this.statusText}`));
+      };
+      xhr.open("GET", url);
+      xhr.send();
+    });
   },
 
   render: function() {
